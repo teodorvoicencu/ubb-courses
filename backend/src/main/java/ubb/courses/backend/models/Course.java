5fea7755c1cecb5d665dc233a603102bcda60cf2
SqrTed <teodorvoicencu@gmail.com>
2019-11-22 23:10:07 +0200

    Create course model

---
+++ backend/src/main/java/ubb/courses/backend/models/Course.java
@@ -0,0 +1,40 @@
+package ubb.courses.backend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import ubb.courses.backend.models.generic.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "courses")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Course extends BaseEntity {
+
+    @Column(name = "name", nullable = false)
+    public String name;
+
+    @Column(name = "description", nullable = false)
+    public String description;
+
+    @Column(name = "created_date", nullable = false, updatable = false)
+    @CreatedDate
+    private long createdDate;
+
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private long modifiedDate;
+
+}
