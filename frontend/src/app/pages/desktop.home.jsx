d0292280970cac80360f94fb58f33690c33b1c0b
Teodor Voicencu <36381859+teodorvoicencu@users.noreply.github.com>
2019-11-24 19:19:08 +0200

    Feature create navbar and routing (#10)
    * Add react router dom and fix folder structure
    
    * Create navbar and navbar routes
    
    * Add lazyloading and create loading spinner for page suspense
    
    - created empty desktop.home page
    - created routing folder for pages
    - enabled react router and configured route switch

---
+++ frontend/src/app/pages/desktop.home.jsx
@@ -0,0 +1,12 @@
+// @flow
+import * as React from 'react';
+
+const HomePage = (): React.Node => {
+    return (
+        <>
+            <h1>This is home</h1>
+        </>
+    );
+};
+
+export default HomePage;
