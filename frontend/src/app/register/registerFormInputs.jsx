1719ccb102eb71e5a9993c5c0d2e6cb18a4c07ab
SqrTed <teodorvoicencu@gmail.com>
2019-11-27 23:01:20 +0200

    Create the register form with inputs and validation

---
+++ frontend/src/app/register/registerFormInputs.jsx
@@ -0,0 +1,78 @@
+// @flow
+import * as React from 'react';
+import { FormikErrors, FormikValues } from 'formik';
+import { Form } from 'react-bootstrap';
+import './styles/registerFormInputs.scss';
+
+type Props = {
+    handleChange: any,
+    values: FormikValues,
+    errors: FormikErrors,
+};
+
+const RegisterFormInputs = (props: Props): React.Node => (
+    <>
+        <Form.Row>
+            <Form.Group controlId="email" className="form-input">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                    type="email"
+                    name="email"
+                    value={props.values.email}
+                    onChange={props.handleChange}
+                    isValid={!props.errors.email && !!props.values.email}
+                    isInvalid={!!props.errors.email && !!props.values.email}
+                />
+                <Form.Control.Feedback type={'invalid'}>{props.errors.email}</Form.Control.Feedback>
+            </Form.Group>
+        </Form.Row>
+        <Form.Row>
+            <Form.Group controlId="authority" className="form-input">
+                <Form.Label>You are a</Form.Label>
+                <Form.Control
+                    as="select"
+                    value={props.values.authority}
+                    onChange={props.handleChange}
+                >
+                    <option value={'STUDENT'}>Student</option>
+                    <option value={'TEACHER'}>Teacher</option>
+                </Form.Control>
+                <Form.Control.Feedback type={'invalid'}>{props.errors.email}</Form.Control.Feedback>
+            </Form.Group>
+        </Form.Row>
+        <Form.Row>
+            <Form.Group controlId="password" className="form-input">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    name="password"
+                    value={props.values.password}
+                    onChange={props.handleChange}
+                    isValid={!props.errors.password && !!props.values.password}
+                    isInvalid={!!props.errors.password && !!props.values.password}
+                />
+                <Form.Control.Feedback type={'invalid'}>
+                    {props.errors.password}
+                </Form.Control.Feedback>
+            </Form.Group>
+        </Form.Row>
+        <Form.Row>
+            <Form.Group controlId="confirmPassword" className="form-input">
+                <Form.Label>Confirm password</Form.Label>
+                <Form.Control
+                    type="password"
+                    name="confirmPassword"
+                    value={props.values.confirmPassword}
+                    onChange={props.handleChange}
+                    isValid={!props.errors.confirmPassword && !!props.values.confirmPassword}
+                    isInvalid={!!props.errors.confirmPassword && !!props.values.confirmPassword}
+                />
+                <Form.Control.Feedback type={'invalid'}>
+                    {props.errors.confirmPassword}
+                </Form.Control.Feedback>
+            </Form.Group>
+        </Form.Row>
+    </>
+);
+
+export default RegisterFormInputs;
