1a95591602d5d4da2158543f314ecae669e18ff5
SqrTed <teodorvoicencu@gmail.com>
2019-12-04 20:48:17 +0200

    Update login form to use the api call

--- frontend/src/app/login/loginForm.jsx
+++ frontend/src/app/login/loginForm.jsx
@@ -2,16 +2,34 @@
 import * as React from 'react';
 import { Formik } from 'formik';
 import { Button, Form } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+import { UserActions } from '../redux/user';
 
 import { loginSchema } from './validation';
 import LoginFormInputs from './loginFormInputs';
 
+import './styles/loginForm.scss';
+
 const LoginForm = (): React.Node => {
-    return (
+    const dispatch = useDispatch();
+    const isLoggedIn = useSelector(state => state.user.loggedIn);
+    const error = useSelector(state => state.user.error);
+
+    const onLogin = React.useCallback(
+        values => {
+            const { email, password } = values;
+            dispatch(UserActions.login(email, password));
+        },
+        [dispatch],
+    );
+
+    return !isLoggedIn ? (
         <Formik
             initialValues={{ email: '', password: '' }}
             validationSchema={loginSchema}
-            onSubmit={() => null}
+            onSubmit={onLogin}
         >
             {({ handleSubmit, handleChange, values, errors }) => (
                 <>
@@ -22,6 +40,7 @@ const LoginForm = (): React.Node => {
                             errors={errors}
                         />
                     </Form>
+                    {error !== null ? <div className={'login-error'}>{error}</div> : null}
                     <div className={'text-center'}>
                         <Button variant={'primary'} type={'submit'} onClick={handleSubmit}>
                             Login
@@ -30,6 +49,8 @@ const LoginForm = (): React.Node => {
                 </>
             )}
         </Formik>
+    ) : (
+        <Redirect to="/" />
     );
 };
 
