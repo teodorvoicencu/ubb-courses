80674e779e6c907da055b7bad6d3cc5b9893d066
SqrTed <teodorvoicencu@gmail.com>
2019-12-04 22:51:29 +0200

    Add register to redux and sagas

--- frontend/src/app/redux/user/user.redux.jsx
+++ frontend/src/app/redux/user/user.redux.jsx
@@ -5,6 +5,10 @@ const { Types, Creators } = createActions({
     loginSuccess: ['data'],
     loginError: ['data'],
     login: ['email', 'password'],
+    registerLoading: ['value'],
+    registerSuccess: [],
+    registerError: ['data'],
+    register: ['email', 'name', 'authority', 'password'],
 });
 
 export const UserTypes = Types;
@@ -14,6 +18,7 @@ const INITIAL_STATE = {
     loading: false,
     error: null,
     loggedIn: false,
+    registered: false,
     accessToken: '',
     tokenType: '',
 };
@@ -22,4 +27,7 @@ export const userReducer = createReducer(INITIAL_STATE, {
     [Types.LOGIN_LOADING]: (state, { value }) => ({ ...state, loading: value }),
     [Types.LOGIN_SUCCESS]: (state, { data }) => ({ ...state, ...data }),
     [Types.LOGIN_ERROR]: (state, { data }) => ({ ...state, error: data }),
+    [Types.REGISTER_LOADING]: (state, { value }) => ({ ...state, loading: value }),
+    [Types.REGISTER_SUCCESS]: state => ({ ...state, registered: true }),
+    [Types.REGISTER_ERROR]: (state, { data }) => ({ ...state, error: data }),
 });
