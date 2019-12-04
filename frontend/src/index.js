86c535e2acdb411396b3b84b57899607ec3f117f
SqrTed <teodorvoicencu@gmail.com>
2019-12-04 20:47:58 +0200

    Add default configs for axios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.scss';
+
 import App from './app/app';
 import * as serviceWorker from './serviceWorker';
+
+axios.defaults.baseURL = 'http://localhost:8080/api';
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.common.Accept = 'application/json';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
