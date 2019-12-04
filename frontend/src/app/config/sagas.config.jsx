368a8a34aad5a8d140ffee217fff482a0eaac876
SqrTed <teodorvoicencu@gmail.com>
2019-12-04 22:52:32 +0200

    Update saga configuration

--- frontend/src/app/config/sagas.config.jsx
+++ frontend/src/app/config/sagas.config.jsx
@@ -1,11 +1,12 @@
 // @flow
-import { all, takeEvery } from 'redux-saga/effects';
+import { all, takeEvery, takeLatest } from 'redux-saga/effects';
 
-import { login, UserTypes } from '../redux/user';
+import { login, register, UserTypes } from '../redux/user';
 
 export default function* rootSaga() {
     try {
         yield all([takeEvery(UserTypes.LOGIN, login)]);
+        yield all([takeLatest(UserTypes.REGISTER, register)]);
     } catch (err) {
         // eslint-disable-next-line no-console
         console.log(err);
