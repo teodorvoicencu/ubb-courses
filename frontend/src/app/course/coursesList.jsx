f74849a1ca28de39f5bd460e771b90caf05df014
Teodor Voicencu <teodorvoicencu@gmail.com>
2020-01-10 20:51:00 +0200

    Create hoc for fetching the courses and injecting them

--- frontend/src/app/course/coursesList.jsx
+++ frontend/src/app/course/coursesList.jsx
@@ -4,35 +4,22 @@ import Row from 'react-bootstrap/Row';
 
 import type { Course } from './types';
 import CourseCard from './courseCard';
+import withCourses from './withCourses';
 
-const mockCourses: Array<Course> = [
-    {
-        id: 1,
-        name: 'Course 1',
-        description: 'Description 1',
-    },
-    {
-        id: 2,
-        name: 'Course 2',
-        description: 'Description 1',
-    },
-    { id: 3, name: 'Course 3', description: 'Description 1' },
-    {
-        id: 4,
-        name: 'Course 4',
-        description: 'Description 1',
-    },
-    { id: 5, name: 'Course 5', description: 'Description 1' },
-];
-
-const CoursesList = (): React.Node => {
-    return mockCourses.map(course => {
-        return (
-            <Row key={course.id}>
-                <CourseCard course={course} />
-            </Row>
-        );
-    });
+type Props = {
+    courses: Course[],
 };
 
-export default CoursesList;
+const CoursesList = ({ courses }: Props): React.Node => {
+    return courses
+        ? courses.map(course => {
+              return (
+                  <Row key={course.id}>
+                      <CourseCard course={course} />
+                  </Row>
+              );
+          })
+        : null;
+};
+
+export default withCourses(CoursesList);
---
+++ frontend/src/app/course/withCourses.jsx
@@ -0,0 +1,52 @@
+// @flow
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+import { CoursesActions, CoursesTypes } from '../redux/courses';
+import SuspenseFallback from '../suspense/suspenseFallback';
+
+import type { Course } from './types';
+
+type CoursesProps = {
+    courses: Course[],
+    loading: boolean,
+};
+
+type Props = {
+    fetchCourses: CoursesTypes.FETCH_COURSES,
+};
+
+const withCourses = <T: *>(
+    component: React.AbstractComponent<*>,
+): React.AbstractComponent<$Diff<T, CoursesProps>> => {
+    class CoursesConnector extends React.Component<Props> {
+        componentDidMount(): void {
+            this.props.fetchCourses();
+        }
+
+        render() {
+            const { loading, courses } = this.props;
+            if (!loading && courses) {
+                return React.createElement(component);
+            }
+            return <SuspenseFallback />;
+        }
+    }
+
+    const mapStateToProps = state => {
+        return {
+            courses: state.courses.data,
+            loading: state.courses.loading,
+        };
+    };
+
+    const mapDispatchToProps = dispatch => {
+        return {
+            fetchCourses: () => dispatch(CoursesActions.fetchCourses()),
+        };
+    };
+
+    return connect(mapStateToProps, mapDispatchToProps)(CoursesConnector);
+};
+
+export default withCourses;
