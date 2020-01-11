c3142db486695c310fa42314db74219fc5ce021d
Teodor Voicencu <teodorvoicencu@gmail.com>
2020-01-11 14:31:44 +0200

    Add fetch course redux and saga

--- frontend/src/app/redux/course/course.sagas.jsx
+++ frontend/src/app/redux/course/course.sagas.jsx
@@ -3,6 +3,20 @@ import axios from 'axios';
 
 import CourseActions from './course.redux';
 
+export function* fetchCourse({ id }) {
+    try {
+        yield put(CourseActions.loading(true));
+        const response = yield call(axios.get, `/courses/${id}`);
+        if (response.status === 200) {
+            yield put(CourseActions.fetchCourseSuccess({ data: response.data }));
+        }
+        yield put(CourseActions.loading(false));
+    } catch (error) {
+        yield put(CourseActions.loading(false));
+        yield put(CourseActions.fetchCourseFailure());
+    }
+}
+
 export function* createCourse({ name, description }) {
     try {
         yield put(CourseActions.loading(true));
--- frontend/src/app/redux/course/index.js
+++ frontend/src/app/redux/course/index.js
@@ -1,4 +1,4 @@
 import CourseActions, { CourseTypes, courseReducer } from './course.redux';
-import { createCourse, updateCourse } from './course.sagas';
+import { createCourse, updateCourse, fetchCourse } from './course.sagas';
 
-export { CourseActions, CourseTypes, courseReducer, createCourse, updateCourse };
+export { CourseActions, CourseTypes, courseReducer, createCourse, updateCourse, fetchCourse };
