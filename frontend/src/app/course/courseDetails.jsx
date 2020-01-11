41b158be08f1e619580072f085bbb5e2f0896001
matt <stroiamatei@gmail.com>
2020-01-11 13:46:53 +0200

    add missing files

---
+++ frontend/src/app/course/courseDetails.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Card } from 'react-bootstrap';
+
+import './styles/courseDetails.scss';
+
+const CourseDetails = (): React.Node => {
+    return (
+        <Card className={'courseDetailsCard shadow-sm'}>
+            <Card.Header as={'h5'}>Description</Card.Header>
+            <Card.Body className={'courseDetails'}>
+                <p>{'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}</p>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default CourseDetails;
