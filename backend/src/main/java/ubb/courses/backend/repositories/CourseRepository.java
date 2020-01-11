a5402b92f2f7b288f0a40a2cabb1438059f1b2ee
axtrm <umihnea52@gmail.com>
2020-01-11 14:56:27 +0200

    Remove unused repository method

--- backend/src/main/java/ubb/courses/backend/repositories/CourseRepository.java
+++ backend/src/main/java/ubb/courses/backend/repositories/CourseRepository.java
@@ -6,5 +6,4 @@ import ubb.courses.backend.models.Course;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Integer> {
-    boolean existsByOwnerIdEqualsAndIdEquals(Integer ownerId, Integer id);
 }
