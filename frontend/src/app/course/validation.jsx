9e387c8a7b3cab7ec8d54797818f50e226606b2e
Teodor Voicencu <teodorvoicencu@gmail.com>
2020-01-13 00:05:58 +0200

    Update course form validation

--- frontend/src/app/course/validation.jsx
+++ frontend/src/app/course/validation.jsx
@@ -3,7 +3,8 @@ import * as yup from 'yup';
 export const addCourseSchema = yup.object().shape({
     name: yup
         .string()
-        .min(3, 'Name is too short')
+        .min(4, 'Name is too short')
+        .max(32, 'Name is too long')
         .required('Name is required'),
     description: yup.string().required('Description is required'),
 });
