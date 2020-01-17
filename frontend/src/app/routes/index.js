234b44ea481e86aa8c7e7fd878e7164d08800378
Teodor Voicencu <teodorvoicencu@gmail.com>
2020-01-17 18:22:09 +0200

    Compact the exports in index js files

---
+++ frontend/src/app/routes/index.js
@@ -0,0 +1,7 @@
+import { RouteTypes } from './types';
+import Routes from './routes';
+import NavRoutes from './navRoutes';
+import TeacherRoute from './teacherRoute';
+import StudentRoute from './studentRoute';
+
+export { Routes, TeacherRoute, StudentRoute, NavRoutes, RouteTypes };
