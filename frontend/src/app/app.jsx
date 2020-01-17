afee6ffc1f293d64e2c3b38210cac103e8b9e193
Teodor Voicencu <teodorvoicencu@gmail.com>
2020-01-17 18:22:29 +0200

    Modify the mapping of routes to take into considerations the type

--- frontend/src/app/app.jsx
+++ frontend/src/app/app.jsx
@@ -4,10 +4,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import NavbarTop from './navbar/navbar';
-import Routes from './routes/routes';
 import SuspenseFallback from './suspense/suspenseFallback';
 import { store } from './config/store.config';
 import { UserActions } from './redux/user';
+import { RouteTypes, Routes, TeacherRoute, StudentRoute } from './routes';
 
 const App = (): React.Node => {
     React.useEffect(() => {
@@ -15,15 +15,13 @@ const App = (): React.Node => {
     });
 
     const routes = Routes.map((route, index) => {
-        return route.component ? (
-            <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                name={route.name}
-                component={route.component}
-            />
-        ) : null;
+        if (route.type === RouteTypes.PRIVATE.TEACHER)
+            return <TeacherRoute key={index} {...route} />;
+
+        if (route.type === RouteTypes.PRIVATE.STUDENT)
+            return <StudentRoute key={index} {...route} />;
+
+        return <Route key={index} {...route} />;
     });
 
     return (
