bdeaf4935059ff2d07f9f08ee57dcc88bec03e94
teodorstrut <49117876+teodorstrut@users.noreply.github.com>
2020-01-18 18:28:10 +0200

    Fixed duplicate enrollment issue (#49)
    * Fixed duplicate enrollment issue
    
    I have fixed an enrollment issue where it would continue enrolling the user in the same course twice
    
    * updated pr
    
    added error for enrolling to your own course

--- backend/src/main/java/ubb/courses/backend/services/course/CourseService.java
+++ backend/src/main/java/ubb/courses/backend/services/course/CourseService.java
@@ -92,7 +92,17 @@ public class CourseService implements ICourseService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new CourseException(String.format("Failed to find user with id=%d.", userId)));
 
+        if (course.getOwner().getId() == userId) {
+            throw new CourseException("You are the owner of the course!" +
+                    " You cannot enlist to your own course! Who would teach it? :P");
+        }
+
+        if (enrollmentRepository.findByCourseIdInAndUserId(courseId, userId) != null) {
+            throw new CourseException(String.format("User already enrolled in course with id=%d.", userId));
+        }
+
         this.enrollmentRepository.save(new Enrollment(course, user));
+
         return course;
     }
 }
