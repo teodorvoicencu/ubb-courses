ce150fcf39130a37f54e26828f5db88e4f8b7ecf
Vlad <konstriker@gmail.com>
2020-01-18 22:01:43 +0200

    Replace bootstrap with react bootstrap

--- frontend/src/app/pages/desktop.homePage.jsx
+++ frontend/src/app/pages/desktop.homePage.jsx
@@ -1,7 +1,7 @@
 // @flow
 import * as React from 'react';
 
-import LandingPage from '../landing/LandingPage';
+import LandingPage from '../landing/landingPage';
 
 const HomePage = (): React.Node => {
     return (
